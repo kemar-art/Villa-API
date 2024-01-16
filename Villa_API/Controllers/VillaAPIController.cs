0d215c3238878b6e6d63735e6bfe33daf8adf064
KEMARNORTON <kemarnorton@hotmail.com>
2024-01-15 20:30:27 -0500

    create the first controller (Get)

---
+++ Villa_API/Controllers/VillaAPIController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Villa_API.Models;
+
+namespace Villa_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VillaAPIController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<Villa> GetVillas()
+        {
+            return new List<Villa> {
+            new() { Id=1,Name= "Test 1"},
+            new() { Id=2,Name= "Test 2"}
+            };
+        }
+    }
+}
