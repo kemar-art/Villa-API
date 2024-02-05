b50cc054102923896989a92c4c247c04833c4d28
KEMARNORTON <kemarnorton@hotmail.com>
2024-02-04 21:44:40 -0500

    Add repository for users

---
+++ Villa_API/Repository/IRepository/IUserRepository.cs
@@ -0,0 +1,12 @@
+﻿using Villa_API.Models;
+using Villa_API.Models.Dto;
+
+namespace Villa_API.Repository.IRepository
+{
+    public interface IUserRepository
+    {
+        bool IsUniqueUser(string username);
+        Task<LoginResponseDTO> Login(LoginRequestDTO loginRequestDTO);
+        Task<LocalUser> Register(RegisterationRequestDTO registerationRequestDTO);
+    }
+}
---
+++ Villa_API/Repository/UserRepository.cs
@@ -0,0 +1,32 @@
+﻿using Villa_API.Data;
+using Villa_API.Models;
+using Villa_API.Models.Dto;
+using Villa_API.Repository.IRepository;
+
+namespace Villa_API.Repository
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public UserRepository(ApplicationDbContext dbContext) 
+        {
+            _dbContext = dbContext;
+        }
+
+        public bool IsUniqueUser(string username)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<LoginResponseDTO> Login(LoginRequestDTO loginRequestDTO)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<LocalUser> Register(RegisterationRequestDTO registerationRequestDTO)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
