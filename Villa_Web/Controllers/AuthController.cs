906274f9dd1f14517eef3b8b566305859ebe0ede
KEMARNORTON <kemarnorton@hotmail.com>
2024-02-14 19:40:42 -0500

    Implement Auth Controllor for register and login

---
+++ Villa_Web/Controllers/AuthController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Villa_Web.Models;
+using Villa_Web.Models.Dto;
+using Villa_Web.Services.IServices;
+
+namespace Villa_Web.Controllers
+{
+    public class AuthController : Controller
+    {
+        private readonly IAuthService _authService;
+
+        public AuthController(IAuthService authService) 
+        {
+            _authService = authService;
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            LoginRequestDTO loginRequestDTO = new();
+            return View(loginRequestDTO);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> login(LoginRequestDTO loginRequestDTO)
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            LoginRequestDTO loginRequestDTO = new();
+            return View(loginRequestDTO);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(RegisterationRequestDTO requestDTO)
+        {
+            APIResponse response = await _authService.RegisterAsync<APIResponse>(requestDTO);
+            if (response != null && response.IsSuccess) 
+            { 
+                return RedirectToAction("Login");
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Logout()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult AccessDenied()
+        {
+            return View();
+        }
+    }
+}
